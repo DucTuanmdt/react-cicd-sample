8b87f894fd8358d5b713796c2245fab82f074810
DucTuanmdt <tuanit381@gmail.com>
2022-08-27 20:25:06 +0700

    modify home page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-import { useState } from "react";
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Welcome to React!!! 😎😍😘🥰</p>
         <a
           className="App-link"
           href="https://reactjs.org"
