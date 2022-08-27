2af624804be36c9d43dd82c6eed97a365f463c7f
DucTuanmdt <tuanit381@gmail.com>
2022-08-27 19:03:59 +0700

    update eslint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useState } from "react";
 
 function App() {
   return (
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Study React
         </a>
       </header>
     </div>
